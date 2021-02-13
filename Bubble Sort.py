274e8a660d5e55d644014352f61f21a326cc05c7
Pavan <golepavan48@gmail.com>
2021-02-13 07:55:01 +0530

    First Commit

---
+++ Bubble Sort.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#Simplest sorting algorithm , comparatively swaps the unsorted data
+#Space Complexity: O(n)
+#Time Complexity : O(n^2)
+def Bubble_Sort(a):
+    for i in range(len(a) - 1):
+        for j in range(len(a) - i -1):
+            if a[j] > a [j + 1]:
+                a[j], a[j+1] = a[j+1], a[j]
+    return a
+                
+
+b = []
+if input("Enter the numbers you want to sort":
+    for i in range(int(input())):
+        b.append(input()) 
+Bubble_Sort(b)
+
+
+
+
